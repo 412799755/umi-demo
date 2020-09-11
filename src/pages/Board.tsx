85f19ea6c2302e6e5d77ced0182a95e2a511c62c
412799755 <412799755@qq.com>
2020-09-11 10:36:04 +0800

    feat: finish all

--- src/pages/Board.tsx
+++ src/pages/Board.tsx
@@ -1,49 +1,26 @@
 import React from 'react';
 import Square from './Square';
 interface IProps {
-  value: Number;
+  squares: Array<any>;
+  // onClick: (i:number)=> any;
+  onClick: (i:number)=> (undefined | null)
 }
 interface State {
-  squares: Array<any>;
-  xIsNext: Boolean;
+
 }
 export default class Board extends React.Component<IProps, State> {
-  constructor(props: any) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-  handleClick(i: number) {
-    const squares = this.state.squares.slice();
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({ squares, xIsNext: !this.state.xIsNext });
-  }
   renderSquare(i: number) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
 
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if (winner) {
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
@@ -64,22 +41,4 @@ export default class Board extends React.Component<IProps, State> {
   }
 }
 
-function calculateWinner(squares: Array<any>) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
+
